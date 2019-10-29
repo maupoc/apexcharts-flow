a762339c7dc56f5f77dfed52dd57b38d63891d48
maupoc <56822746+maupoc@users.noreply.github.com>
2019-10-29 15:40:03 +0100

    remove unsupported scatter

--- src/main/java/com/github/appreciated/apexcharts/config/series/SeriesType.java
+++ src/main/java/com/github/appreciated/apexcharts/config/series/SeriesType.java
@@ -3,8 +3,7 @@ package com.github.appreciated.apexcharts.config.series;
 public enum SeriesType {
 	line("line"),
     area("area"),
-    column("column"),
-    scatter("scatter");
+    column("column");
     private final String name;
 
     SeriesType(String name) {
